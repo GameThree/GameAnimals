6eb618b6caf763298a988ee93607ce419a30da5b
coderflyfish <277802345@qq.com>
2017-02-26 22:02:45 +0800

    修改

---
+++ Assets/Script/Boot.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Boot : MonoBehaviour 
+{
+    private GameCore m_game;
+
+    void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+        if (m_game == null)
+        {
+            m_game = gameObject.AddComponent<GameCore>();
+        }
+    }
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/Script/GameCore.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameCore : MonoBehaviour
+{
+    private static GameCore instance = null;
+
+    public static GameCore Instance()
+    {
+        return instance;
+    }
+
+    void Awake()
+    {
+        instance = this;
+    }
+	// Use this for initialization
+	void Start ()
+    {
+
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	  
+	}
+}
