890667e1eb7594265fde49265d25bfd486e50f2c
coderflyfish <277802345@qq.com>
2018-05-02 21:27:13 +0800

    目录调整

---
+++ UnityPrj/Assets/Script/NewBehaviourScript.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Net.Sockets;
+using System.Threading;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+    public Camera camera;
+
+    private void OnMouseDrag()
+    {
+        if (Camera.main!=null)
+        {
+            Vector3 mousPos1 = new Vector3(Input.mousePosition.x, Input.mousePosition.y, -Camera.main.transform.position.z);
+            Vector3 mousPos = new Vector3(Input.mousePosition.x, Input.mousePosition.y, transform.position.z - Camera.main.transform.position.z);
+            Vector3 objPosition = Camera.main.ScreenToWorldPoint(mousPos);
+            Vector3 objPosition1 = Camera.main.ScreenToWorldPoint(mousPos1);
+            //transform.position = objPosition1;
+            var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            //RaycastHit hitInfo;
+            //Physics.Raycast(ray, out hitInfo, 1000, LayerMask.GetMask("Scene"));
+            Debug.DrawRay(ray.origin, ray.direction * 1000);
+        }
+        else
+        {
+            var ray1 = camera.ScreenPointToRay(Input.mousePosition);
+            Debug.DrawRay(ray1.origin, ray1.direction * 10000);
+        }
+
+
+        //Debug.Log(mousPos + "         " + objPosition + "              " + (objPosition - Camera.main.transform.position) + "   " + objPosition1 + "  " + hitInfo.point);
+    }
+
+    // Use this for initialization
+    void Start () {
+        NewBehaviourScript1.Connect();
+
+       // StartCoroutine(Recive());
+    }
+	
+    void Recive()
+    {
+
+    }
+	// Update is called once per frame
+	void Update ()
+    {
+        if(NewBehaviourScript1.recives != null&& NewBehaviourScript1.recives.Length>0)
+        {
+            Debug.Log(NewBehaviourScript1.recives);
+            NewBehaviourScript1.recives = null;
+        }
+    }
+}
---
+++ UnityPrj/Assets/Script/NewBehaviourScript1.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Net.Sockets;
+using System.Threading;
+using UnityEngine;
+
+public class NewBehaviourScript1
+{
+    public static Socket m_socket;
+    static Thread threadReceive;
+    public static byte[] recives;
+
+    public static void Connect()
+    {
+        m_socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        m_socket.Connect("127.0.0.1", 20001);
+        //开启一个新的线程不停的接收服务器发送消息的线程
+        threadReceive = new Thread(new ThreadStart(Receive));
+        //设置为后台线程
+        threadReceive.IsBackground = true;
+        threadReceive.Start();
+    }
+    public static void Receive()
+    {
+        while (true)
+        {
+            if (m_socket.Connected)
+            {
+                byte[] recive = new byte[1024];
+                int count = m_socket.Receive(recive);
+                if (count > 0)
+                {
+                    recives = recive;
+                }
+                System.Console.Out.WriteLine("aaaaaaaaaaa" + count);
+            }
+        }
+    }
+}
